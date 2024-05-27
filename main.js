ac49e4027751c9445189923900a62c63e47dd4c3
Arun <apy1412@gmail.com>
2024-05-27 12:40:52 +0400

    functionality added with pulling data from teh server

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const temperature = document.querySelector(".temperature") 
+const template = document.querySelector("#petTemplate")
+const wrapper = document.createDocumentFragment()
 async function getWeather() {
     const weatherPromise = await fetch("https://api.weather.gov/gridpoints/OKX/27,35/forecast")
     const weatherData = await weatherPromise.json()
@@ -8,3 +10,33 @@ async function getWeather() {
 
 
 getWeather()
+
+async function getPetData() {
+    const petDataPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
+    const petData = await petDataPromise.json()
+   
+    petData.forEach(pet =>{
+        const duplicate = template.content.cloneNode(true)
+        duplicate.querySelector("h3").textContent = pet.name
+        duplicate.querySelector(".description").textContent = pet.description
+        duplicate.querySelector(".age").textContent = updateAge(pet.birthYear)
+        if(!pet.photo) pet.photo = "images/fallback.jpg"
+        duplicate.querySelector(".pet__image img").src = pet.photo
+        duplicate.querySelector(".pet__image img").alt = `${pet.species} name ${pet.name}`
+        wrapper.appendChild(duplicate)
+    })
+    document.querySelector(".petList__grid").appendChild(wrapper)
+    
+}
+getPetData()
+
+function updateAge(birthYear){
+    const currentYear = new Date().getFullYear()
+    const age = currentYear - birthYear
+    if(age ==1) return "1 year Old"
+    if(age ==0) return "less than a year old"
+    return `${age} years old`
+    
+
+    
+}
